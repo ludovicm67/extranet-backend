3856e90191e4cf2945de410160c0e0d9ad1b0b83
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-09 11:12:10 +0200

    added custom 404 and 503 errors

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+{!! json_encode([
+  'success' => false,
+  'code' => 404,
+  'message' => 'Service action not found']
+) !!}
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,5 @@
+{!! json_encode([
+  'success' => false,
+  'code' => 503,
+  'message' => 'Service currently in maintenance']
+) !!}
