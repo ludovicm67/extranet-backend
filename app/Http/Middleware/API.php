53acf77184f0b4bdb1c1e6500a2de057d2f4de22
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-12 11:49:27 +0200

    added response headers

--- app/Http/Middleware/API.php
+++ app/Http/Middleware/API.php
@@ -9,6 +9,8 @@ class API {
   {
     $response = $next($request);
     $response->header('Access-Control-Allow-Origin', '*');
+    $response->header('Access-Control-Allow-Headers', 'X-Requested-With');
+    $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     return $response;
   }
 }
