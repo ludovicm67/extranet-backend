fac4215011e05a773eeb73575c387e9627655646
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-23 11:56:21 +0200

    clean code

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -41,41 +41,7 @@ class ProjectController extends Controller
       return $tag->id;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $validator = Validator::make($request->all(), [
-        'name' => 'required|string|max:255',
-      ]);
-
-      if ($validator->fails()) {
-        return response()->json([
-          'success' => false,
-          'errors' => $validator->errors()->all(),
-        ], 400);
-      }
-
-      // fields that can be nullable
-      $domain = empty($request->domain) ? null : $request->domain;
-      $clientId = empty($request->client_id) ? null : $request->client_id;
-      $nextAction = empty($request->next_action) ? null : $request->next_action;
-      $endAt = $request->end_at
-        ? date('Y-m-d', strtotime($request->end_at))
-        : null;
-
-      $project = Project::create([
-        'name' => $request->name,
-        'domain' => $domain,
-        'client_id' => $clientId,
-        'next_action' => $nextAction,
-        'end_at' => $endAt,
-      ]);
-
+    private function createAssociations(Request $request, Project $project) {
       if (!empty($request->contacts)) {
         foreach (array_unique($request->contacts) as $contact) {
           ProjectContact::create([
@@ -128,6 +94,44 @@ class ProjectController extends Controller
           ]);
         }
       }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+      ]);
+
+      if ($validator->fails()) {
+        return response()->json([
+          'success' => false,
+          'errors' => $validator->errors()->all(),
+        ], 400);
+      }
+
+      // fields that can be nullable
+      $domain = empty($request->domain) ? null : $request->domain;
+      $clientId = empty($request->client_id) ? null : $request->client_id;
+      $nextAction = empty($request->next_action) ? null : $request->next_action;
+      $endAt = $request->end_at
+        ? date('Y-m-d', strtotime($request->end_at))
+        : null;
+
+      $project = Project::create([
+        'name' => $request->name,
+        'domain' => $domain,
+        'client_id' => $clientId,
+        'next_action' => $nextAction,
+        'end_at' => $endAt,
+      ]);
+
+      $this->createAssociations($request, $project);
 
       return response()->json([
         'success' => true,
@@ -188,58 +192,7 @@ class ProjectController extends Controller
         'end_at' => $endAt,
       ]);
 
-      if (!empty($request->contacts)) {
-        foreach (array_unique($request->contacts) as $contact) {
-          ProjectContact::create([
-            'project_id' => $project->id,
-            'contact_id' => intval($contact, 10),
-          ]);
-        }
-      }
-
-      if (!empty($request->orders) && is_array($request->orders)) {
-        foreach (array_unique($request->orders) as $order) {
-          ProjectOrder::create([
-            'project_id' => $project->id,
-            'order_id' => intval($order, 10),
-          ]);
-        }
-      }
-
-      if (!empty($request->users) && is_array($request->users)) {
-        foreach (array_unique($request->users) as $user) {
-          ProjectUser::create([
-            'project_id' => $project->id,
-            'user_id' => intval($user, 10),
-          ]);
-        }
-      }
-
-      if (!empty($request->tags) && is_array($request->tags)) {
-        foreach ($request->tags as $tag) {
-          if (empty($tag['id'])) continue;
-          $tagId = $this->createTagsOnThFly($tag['id']);
-          if (empty($tagId)) continue;
-          ProjectTag::create([
-            'project_id' => $project->id,
-            'tag_id' => $tagId,
-            'value' => $tag['value'],
-          ]);
-        }
-      }
-
-      if (!empty($request->urls) && is_array($request->urls)) {
-        $orderUrl = 0;
-        foreach ($request->urls as $url) {
-          if (empty($url['name']) && empty($url['value'])) continue;
-          ProjectUrl::create([
-            'project_id' => $project->id,
-            'name' => $url['name'],
-            'value' => $url['value'],
-            'order' => $orderUrl++,
-          ]);
-        }
-      }
+      $this->createAssociations($request, $project);
 
       return response()->json([
         'success' => true,
