c7b783255634630ea9a7b7befedad5a38ffa3125
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-25 11:17:56 +0200

    finished contracts controller

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Contract;
 use Illuminate\Http\Request;
 
@@ -28,7 +29,36 @@ class ContractController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $validator = Validator::make($request->all(), [
+        'user_id' => 'exists:users,id',
+        'type' => 'required|string',
+        'start_at' => 'required',
+      ]);
+
+      if ($validator->fails()) {
+        return response()->json([
+          'success' => false,
+          'errors' => $validator->errors()->all(),
+        ], 400);
+      }
+
+      $startAt = $request->start_at
+        ? date('Y-m-d', strtotime($request->start_at))
+        : null;
+      $endAt = $request->end_at
+        ? date('Y-m-d', strtotime($request->end_at))
+        : null;
+
+      Contract::create([
+        'user_id' => $request->user_id,
+        'type' => $request->type,
+        'start_at' => $startAt,
+        'end_at' => $endAt,
+      ]);
+
+      return response()->json([
+        'success' => true,
+      ]);
     }
 
     /**
@@ -39,7 +69,10 @@ class ContractController extends Controller
      */
     public function show(Contract $contract)
     {
-        //
+      return response()->json([
+        'success' => true,
+        'data' => $contract,
+      ]);
     }
 
     /**
@@ -51,7 +84,36 @@ class ContractController extends Controller
      */
     public function update(Request $request, Contract $contract)
     {
-        //
+      $validator = Validator::make($request->all(), [
+        'user_id' => 'exists:users,id',
+        'type' => 'required|string',
+        'start_at' => 'required',
+      ]);
+
+      if ($validator->fails()) {
+        return response()->json([
+          'success' => false,
+          'errors' => $validator->errors()->all(),
+        ], 400);
+      }
+
+      $startAt = $request->start_at
+        ? date('Y-m-d', strtotime($request->start_at))
+        : null;
+      $endAt = $request->end_at
+        ? date('Y-m-d', strtotime($request->end_at))
+        : null;
+
+      $contract->update([
+        'user_id' => $request->user_id,
+        'type' => $request->type,
+        'start_at' => $startAt,
+        'end_at' => $endAt,
+      ]);
+
+      return response()->json([
+        'success' => true,
+      ]);
     }
 
     /**
