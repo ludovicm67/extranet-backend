1212d3b60e1f234b881c78138e3ac348d4f47139
ludovicm67 <ludovicmuller1@gmail.com>
2018-07-27 12:02:37 +0200

    can create leave

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Leave;
 use Illuminate\Http\Request;
 
@@ -28,7 +29,47 @@ class LeaveController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $validator = Validator::make($request->all(), [
+        'reason' => 'required|string',
+        'file' => 'nullable|file',
+        'start' => 'required|string',
+        'start_time' => 'required|integer|min:0|max:23',
+        'end' => 'required|string',
+        'end_time' => 'required|integer|min:0|max:23',
+      ]);
+
+      if ($validator->fails()) {
+        return response()->json([
+          'success' => false,
+          'errors' => $validator->errors()->all(),
+        ], 400);
+      }
+
+      $file = $request->file('file');
+      if (!empty($file)) {
+        $file = str_replace('public/', '', $file->store('public/leave/' . date('Y') . '/' . date('n')));
+      }
+
+      $startDate = date(
+        'Y-m-d H:i:s',
+        strtotime($request->start . ' ' . $request->start_time . ':00:00')
+      );
+      $endDate = date(
+        'Y-m-d H:i:s',
+        strtotime($request->end . ' ' . $request->end_time . ':00:00')
+      );
+
+      Leave::create([
+        'user_id' => auth()->user()->id,
+        'accepted' => 0,
+        'file' => $file,
+        'details' => $request->details,
+        'start' => $startDate,
+        'start_time' => $request->start_time,
+        'end' => $endDate,
+        'end_time' => $request->end_time,
+        'reason' => $request->reason,
+      ]);
     }
 
     /**
