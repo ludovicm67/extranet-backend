cc331bd9d6534ab0ce36eec4b65b4613a3db3c27
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-01 11:05:16 +0200

    clean routes

--- routes/web.php
+++ routes/web.php
@@ -2,44 +2,34 @@
 
 Route::group(['middleware' => 'api'], function () {
 
+  // @TODO: move it in the auth middleware!
+  Route::get('projects/{project}/identifiers', 'ProjectController@identifiers')->name('projects.identifiers');
+
   // routes for cron jobs
-  Route::get('cron/sellsy_clients', 'CronController@sellsy_clients')
-        ->name('cron.sellsy_clients');
-  Route::get('cron/sellsy_contacts', 'CronController@sellsy_contacts')
-        ->name('cron.sellsy_contacts');
-  Route::get('cron/sellsy_orders', 'CronController@sellsy_orders')
-        ->name('cron.sellsy_orders');
-  Route::get('cron/sellsy_invoices', 'CronController@sellsy_invoices')
-        ->name('cron.sellsy_invoices');
+  Route::get('cron/sellsy_clients', 'CronController@sellsy_clients')->name('cron.sellsy_clients');
+  Route::get('cron/sellsy_contacts', 'CronController@sellsy_contacts')->name('cron.sellsy_contacts');
+  Route::get('cron/sellsy_orders', 'CronController@sellsy_orders')->name('cron.sellsy_orders');
+  Route::get('cron/sellsy_invoices', 'CronController@sellsy_invoices')->name('cron.sellsy_invoices');
 
   Route::get('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
   Route::post('auth/login', 'AuthController@login')->name('auth.login');
-  Route::post('password/reset', 'UserController@resetPassword')
-        ->name('password.reset');
+  Route::post('password/reset', 'UserController@resetPassword')->name('password.reset');
 
   Route::group(['middleware' => ['jwt.auth']], function() {
-    Route::match(['PUT', 'PATCH'], 'users/me', 'UserController@updateMe')
-          ->name('users.update_me');
-    Route::get('users/me', 'AuthController@me')
-          ->name('users.me');
+    Route::match(['PUT', 'PATCH'], 'users/me', 'UserController@updateMe')->name('users.update_me');
+    Route::get('users/me', 'AuthController@me')->name('users.me');
 
     Route::get('pdf/compta', 'PDFController@compta')->name('pdf.compta');
 
     Route::get('requests', 'RequestController@index')->name('requests.index');
 
-    Route::post('leave/{leave}/accept', 'LeaveController@accept')
-          ->name('leave.accept');
-    Route::post('leave/{leave}/reject', 'LeaveController@reject')
-          ->name('leave.reject');
-    Route::post('expenses/{expense}/accept', 'ExpenseController@accept')
-          ->name('expenses.accept');
-    Route::post('expenses/{expense}/reject', 'ExpenseController@reject')
-          ->name('expenses.reject');
+    Route::post('leave/{leave}/accept', 'LeaveController@accept')->name('leave.accept');
+    Route::post('leave/{leave}/reject', 'LeaveController@reject')->name('leave.reject');
+    Route::post('expenses/{expense}/accept', 'ExpenseController@accept')->name('expenses.accept');
+    Route::post('expenses/{expense}/reject', 'ExpenseController@reject')->name('expenses.reject');
 
-    Route::post('projects/{project}/fav', 'ProjectController@fav')
-          ->name('projects.fav');
-    Route::post('projects/{project}/unfav', 'ProjectController@unfav')
-          ->name('projects.unfav');
+    Route::post('projects/{project}/fav', 'ProjectController@fav')->name('projects.fav');
+    Route::post('projects/{project}/unfav', 'ProjectController@unfav')->name('projects.unfav');
 
     Route::model('client', \App\SellsyClient::class);
     Route::model('sellsy_client', \App\SellsyClient::class);
@@ -64,7 +54,7 @@ Route::group(['middleware' => 'api'], function () {
     Route::get('auth/logout', 'AuthController@logout')->name('auth.logout');
     Route::get('team', 'UserController@team')->name('users.team');
 
-
+    // avoid strange behaviors
     Route::options('{all}', function () {
       $response = Response::make('');
 
