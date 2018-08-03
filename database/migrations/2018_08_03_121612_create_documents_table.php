5841ded6455f4bc59a189c058bff5d11065a0ae4
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-03 14:22:29 +0200

    create documents table

---
+++ database/migrations/2018_08_03_121612_create_documents_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('type')->nullable(); // 'pay', 'medical', ...
+            $table->dateTime('date')->useCurrent();
+            $table->string('file');
+            $table->string('details')->nullable();
+            $table->timestamps();
+
+            $table
+              ->foreign('user_id')
+              ->references('id')
+              ->on('users')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
