68806ff7d1b56bcfe7b190c24ec0cc0fc5514e02
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-07 11:17:57 +0200

    search controller is ready

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use App\User;
+use App\SellsyClient;
+use App\SellsyContact;
+use App\Project;
+use App\Contact;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -11,10 +17,26 @@ class SearchController extends Controller
     $search = '%' . $request->q . '%';
     $users = User::where('firstname', 'like', $search)
               ->orWhere('lastname', 'like', $search)
+              ->orWhere(DB::raw("concat(firstname, ' ', lastname)"), 'like', $search)
+              ->orWhere(DB::raw("concat(lastname, ' ', firstname)"), 'like', $search)
               ->orWhere('email', 'like', $search)->get();
+    $clients = SellsyClient::where('fullName', 'like', $search)->get();
+    $sellsyContacts = SellsyContact::where('fullName', 'like', $search)
+                      ->orWhere('email', 'like', $search)
+                      ->with('client')->get();
+    $projects = Project::where('name', 'like', $search)
+                ->orWhere('domain', 'like', $search)->get();
+    $contacts = Contact::where('name', 'like', $search)->get();
+    $tags = Tag::where('name', 'like', $search)->get();
+
 
     return response()->json([
       'users' => $users,
+      'clients' => $clients,
+      'projects' => $projects,
+      'contacts' => $contacts,
+      'sellsy_contacts' => $sellsyContacts,
+      'tags' => $tags,
     ]);
   }
 }
