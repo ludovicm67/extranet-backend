c955e77a0c271e8e8e131386723794b5205cdf30
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-08 15:43:37 +0200

    add team field on users

---
+++ database/migrations/2018_08_08_133205_add_team_id_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTeamIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->unsignedInteger('team_id')->nullable();
+          $table->foreign('team_id')
+                ->references('id')
+                ->on('teams')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+          $table->dropForeign(['team_id']);
+          $table->dropColumn('team_id');
+        });
+    }
+}
