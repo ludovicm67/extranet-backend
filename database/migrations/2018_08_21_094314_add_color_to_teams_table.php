021c6cb3ced2874c248ac5ac3c9e390aa0a09cbc
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-21 11:45:40 +0200

    added color field for teams

---
+++ database/migrations/2018_08_21_094314_add_color_to_teams_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColorToTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('color')->default('#000000');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn('color');
+        });
+    }
+}
